58a8bd42c03a6ec33c86652254d096fbb83a16f7
Samuel Marks <samuelbmarks@gmail.com>
2023-07-07 18:56:38 -0500

    Tried to remove all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fun-personal-website';
+
+  constructor() { }
+
+  ngOnInit() {
+    // document.documentElement.style.setProperty('--gradientColor1', "#000000");
+    // document.documentElement.style.setProperty('--gradientColor1', "#FFFFFF");
+    document.documentElement.style.setProperty('--gradientColor1', "#060B0B");
+    document.documentElement.style.setProperty('--gradientColor2', "#0C1515");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// angular material
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
