be512527d284eeea3d40d68431b8948d724bac3f
Samuel Marks <samuelbmarks@gmail.com>
2023-08-27 17:25:19 -0500

    Link updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,11 +21,6 @@ export class AppComponent implements OnInit {
     document.documentElement.style.setProperty('--color2', "#C8C5FB");
     document.documentElement.style.setProperty('--color3', "#DEE0FD");
 
-    document.documentElement.style.setProperty('--tab1', "#7B76B1");
-    document.documentElement.style.setProperty('--tab2', "#8F88C9");
-    document.documentElement.style.setProperty('--tab3', "#A39AE1");
-    document.documentElement.style.setProperty('--tab4', "#B8ACF9");
-
     document.documentElement.style.setProperty('--bean', "#DEE0FD");
     document.documentElement.style.setProperty('--bean-hover', "#B8ACF9");
 
@@ -33,34 +28,12 @@ export class AppComponent implements OnInit {
     document.documentElement.style.setProperty('--theme1', "#AA8996");
     document.documentElement.style.setProperty('--theme1-mono', "#956B7C");
 
-    // theme2 : green
-    document.documentElement.style.setProperty('--theme3', "#AA8996");
-    document.documentElement.style.setProperty('--theme3-mono', "#956B7C");
-
-    // theme3 : yellow
-    document.documentElement.style.setProperty('--theme4', "#AA8996");
-    document.documentElement.style.setProperty('--theme4-mono', "#956B7C");
-
-    // theme4 : purple
-    document.documentElement.style.setProperty('--theme5', "#AA8996");
-    document.documentElement.style.setProperty('--theme5-mono', "#956B7C");
-
-
     // logo
     document.documentElement.style.setProperty('--theme2', "#FEFEFE");
-    document.documentElement.style.setProperty('--theme2-dark1', "#F2F1F1");
     document.documentElement.style.setProperty('--theme2-dark2', "#E6E4E5");
-    document.documentElement.style.setProperty('--theme2-dark3', "#DBD6D8");
     document.documentElement.style.setProperty('--theme2-dark4', "#D0C8CB");
-    document.documentElement.style.setProperty('--theme2-dark5', "#C6B9BE");
-    document.documentElement.style.setProperty('--theme2-dark6', "#BCA9B1");
 
-    document.documentElement.style.setProperty('--theme2-darker1', "#020102");
-    document.documentElement.style.setProperty('--theme2-darker2', "#171214");
-    document.documentElement.style.setProperty('--theme2-darker3', "#2C2327");
-    document.documentElement.style.setProperty('--theme2-darker4', "#42343A");
-    document.documentElement.style.setProperty('--theme2-darker5', "#57454D");
-    document.documentElement.style.setProperty('--theme2-darker6', "#6C565F");
+    document.documentElement.style.setProperty('--theme2-black', "#020102");
 
     // wait times for color transitions
     
