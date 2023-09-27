c628f7a61ab7a055f4afb8f0a01c6a432af581ba
Samuel Marks <samuelbmarks@gmail.com>
2023-09-27 15:32:00 -0500

    Clean-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,27 +14,8 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   async ngOnInit() {
-    document.documentElement.style.setProperty('--gradientColor1', "#060B0B");
-    document.documentElement.style.setProperty('--gradientColor2', "#0C1515");
-
-    document.documentElement.style.setProperty('--color1', "#B8ACF9");
-    document.documentElement.style.setProperty('--color2', "#C8C5FB");
-    document.documentElement.style.setProperty('--color3', "#DEE0FD");
-
-    document.documentElement.style.setProperty('--bean', "#DEE0FD");
-    document.documentElement.style.setProperty('--bean-hover', "#B8ACF9");
-
-    // theme1 : red
-    document.documentElement.style.setProperty('--theme1', "#AA8996");
-    document.documentElement.style.setProperty('--theme1-mono', "#956B7C");
-
-    // logo
-    document.documentElement.style.setProperty('--theme2', "#FEFEFE");
-    document.documentElement.style.setProperty('--theme2-dark2', "#E6E4E5");
-    document.documentElement.style.setProperty('--theme2-dark4', "#D0C8CB");
-
-    document.documentElement.style.setProperty('--theme2-black', "#020102");
-
+    document.documentElement.style.setProperty('--app-logo-color', "white");
+    
     // animations
     document.documentElement.style.setProperty('--a-scale-duration', "0.6s");
     document.documentElement.style.setProperty('--a-scale-timing-function', "ease");
