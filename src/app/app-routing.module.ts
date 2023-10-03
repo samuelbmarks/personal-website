751e5e6f8577184105fed34485bfa30babd993b0
Samuel Marks <samuelbmarks@gmail.com>
2023-10-03 16:13:12 -0500

    Added hive redirection routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hive',
+    redirectTo: 'https://hivepersonaltraining.com',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
