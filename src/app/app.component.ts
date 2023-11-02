9a75c17c46ab94cf6d3e82625520e46511117316
Samuel Marks <samuelbmarks@gmail.com>
2023-11-01 19:53:20 -0500

    Cleaned up theme logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,20 @@ export class AppComponent implements OnInit {
 
   private currentTheme = "";
 
+  private theme1 = "";
+  private theme2 = "";
+  private theme3 = "";
+  private theme4 = "";
+
   ngOnInit() {
     this.changeTheme("light");
     
     // animations
+
     document.documentElement.style.setProperty('--a-scale-duration', "0.6s");
-    document.documentElement.style.setProperty('--a-scale-timing-function', "ease");    
+    document.documentElement.style.setProperty('--a-scale-timing-function', "ease");
+
+    this.updateThemeProperties();
   }
 
   public changeTheme(theme: string) {
@@ -28,73 +36,19 @@ export class AppComponent implements OnInit {
     else if (theme == "light") {
       this.currentTheme = "light";
 
-      document.documentElement.style.setProperty('--bkgd', "rgba(255, 255, 255, 1)");
-
-      document.documentElement.style.setProperty('--bar', "rgba(255, 255, 255, 1)");
-      // document.documentElement.style.setProperty('--bar-hover', "#E7E7E7");
-
-      document.documentElement.style.setProperty('--jcms-bkgd', "black");
-      document.documentElement.style.setProperty('--jcms-bkgd-hover', "#7794C4");
-      document.documentElement.style.setProperty('--jcms-letter', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--jcms-letter-hover', "rgba(241, 241, 241, 1)");
-
-      document.documentElement.style.setProperty('--whale1', "black");
-      document.documentElement.style.setProperty('--whale2', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--whale1-hover', "#7794C4");
-      document.documentElement.style.setProperty('--whale2-hover', "rgba(241, 241, 241, 1)");
-
-      // document.documentElement.style.setProperty('--button-shadow', "0px 0px 10px rgba(0, 0, 0, 0.4)");
-      document.documentElement.style.setProperty('--button-text', "black");
-      document.documentElement.style.setProperty('--button-svg', "black");
-      document.documentElement.style.setProperty('--button-bkgd', "#E7E7E7");
-      // document.documentElement.style.setProperty('--button-bkgd', "rgba(241, 241, 241, 1)");
-
-      document.documentElement.style.setProperty('--button-bkgd-hover', "#7794C4");
-      document.documentElement.style.setProperty('--button-svg-hover', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--button-text-hover', "rgba(241, 241, 241, 1)");
-      
-      document.documentElement.style.setProperty('--button-bkgd-selected', "black");
-      document.documentElement.style.setProperty('--button-svg-selected', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--button-text-selected', "rgba(241, 241, 241, 1)");
-
-      document.documentElement.style.setProperty('--content-text', "black");
+      this.theme1 = "#F1F1F1";
+      this.theme2 = "#000000";
+      this.theme3 = "#E0E0E0";
+      this.theme4 = "#9296F0";
     }
 
     else if (theme == "dark") {
       this.currentTheme = "dark";
 
-      // #170000
-
-      document.documentElement.style.setProperty('--bkgd', "#000000");
-
-      document.documentElement.style.setProperty('--bar', "#000000");
-      // document.documentElement.style.setProperty('--bar-hover', "#4B4A54");
-
-      document.documentElement.style.setProperty('--jcms-bkgd', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--jcms-letter', "black");
-      document.documentElement.style.setProperty('--jcms-bkgd-hover', "#7794C4");
-      document.documentElement.style.setProperty('--jcms-letter-hover', "rgba(241, 241, 241, 1)");
-
-      document.documentElement.style.setProperty('--whale1', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--whale2', "black");
-      document.documentElement.style.setProperty('--whale1-hover', "black");
-      document.documentElement.style.setProperty('--whale2-hover', "rgba(241, 241, 241, 1)");
-      
-      // document.documentElement.style.setProperty('--button-shadow', "0px 0px 10px rgba(241, 241, 241, 1, 0.4)");
-      // document.documentElement.style.setProperty('--button-shadow', "0px 0px 10px rgba(255, 255, 255, 0.2)");
-      document.documentElement.style.setProperty('--button-text', "white");
-      document.documentElement.style.setProperty('--button-svg', "white");
-      document.documentElement.style.setProperty('--button-bkgd', "#343738");
-
-      document.documentElement.style.setProperty('--button-bkgd-hover', "#7794C4");
-      document.documentElement.style.setProperty('--button-svg-hover', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--button-text-hover', "rgba(241, 241, 241, 1)");
-      
-      document.documentElement.style.setProperty('--button-bkgd-selected', "rgba(241, 241, 241, 1)");
-      document.documentElement.style.setProperty('--button-svg-selected', "black");
-      document.documentElement.style.setProperty('--button-text-selected', "black");
-      
-      document.documentElement.style.setProperty('--content-text', "rgba(241, 241, 241, 1)");
+      this.theme1 = "#000000";
+      this.theme2 = "#F1F1F1";
+      this.theme3 = "#343738";
+      this.theme4 = "#9296F0";
     }
 
     else if (theme == "pinto") {
@@ -112,5 +66,39 @@ export class AppComponent implements OnInit {
     else if (theme == "yoshi") {
       this.currentTheme = "yoshi";
     }
+
+    this.updateThemeProperties();
+  }
+
+  private updateThemeProperties() {
+    document.documentElement.style.setProperty('--bkgd', this.theme1);
+
+    document.documentElement.style.setProperty('--bar', this.theme1);
+    // document.documentElement.style.setProperty('--bar-hover', "#E7E7E7");
+
+    document.documentElement.style.setProperty('--jcms-bkgd', this.theme2);
+    document.documentElement.style.setProperty('--jcms-bkgd-hover', this.theme4);
+    document.documentElement.style.setProperty('--jcms-letter', this.theme1);
+    document.documentElement.style.setProperty('--jcms-letter-hover', this.theme1);
+
+    document.documentElement.style.setProperty('--whale1', this.theme2);
+    document.documentElement.style.setProperty('--whale2', this.theme1);
+    document.documentElement.style.setProperty('--whale1-hover', this.theme4);
+    document.documentElement.style.setProperty('--whale2-hover', this.theme1);
+
+    // document.documentElement.style.setProperty('--button-shadow', "0px 0px 10px rgba(0, 0, 0, 0.4)");
+    document.documentElement.style.setProperty('--button-text', this.theme2);
+    document.documentElement.style.setProperty('--button-svg', this.theme2);
+    document.documentElement.style.setProperty('--button-bkgd', this.theme3);
+
+    document.documentElement.style.setProperty('--button-bkgd-hover', this.theme4);
+    document.documentElement.style.setProperty('--button-svg-hover', "#FFFFFF");
+    document.documentElement.style.setProperty('--button-text-hover', "#FFFFFF");
+    
+    document.documentElement.style.setProperty('--button-bkgd-selected', this.theme2);
+    document.documentElement.style.setProperty('--button-svg-selected', this.theme1);
+    document.documentElement.style.setProperty('--button-text-selected', this.theme1);
+
+    document.documentElement.style.setProperty('--content-text', this.theme2);
   }
 }
